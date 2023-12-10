170a701c97b4cdbfffa2d386502128e74cec4289
Paul Mazzia <p.mazzia63@gmail.com>
2023-12-10 20:37:24 +0100

    update fluid properties class

--- pyfluid/simulation_configuration/fluid_properties/fluid_properties.py
+++ pyfluid/simulation_configuration/fluid_properties/fluid_properties.py
@@ -1,2 +1,19 @@
 class FluidProperties:
-    pass
+    def __init__(self,
+                 temperature: float,
+                 pressure: float,
+                 speed: float
+                 ) -> None:
+        """
+        Initializes the FluidProperties class with the given parameters.
+
+        :param temperature: The temperature of the fluid.
+        :type temperature: float
+        :param pressure: The pressure of the fluid.
+        :type pressure: float
+        :param speed: The speed of the fluid.
+        :type speed: float
+        """
+        self.temperature = temperature
+        self.pressure = pressure
+        self.speed = speed
