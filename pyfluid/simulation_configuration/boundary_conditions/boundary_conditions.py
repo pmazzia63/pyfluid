3a123967d1fbf28ad8619e6597c0938904bc9415
Paul Mazzia <p.mazzia63@gmail.com>
2023-12-10 16:36:11 +0100

    Implement the boundary_conditions class

--- pyfluid/simulation_configuration/boundary_conditions/boundary_conditions.py
+++ pyfluid/simulation_configuration/boundary_conditions/boundary_conditions.py
@@ -1,2 +1,46 @@
+from typing import Dict
+
+
+BOUNDARY_CONDITIONS_TYPE = ['pressure', 'speed']
+
+
 class BoundaryConditions:
-    pass
+    def __init__(self,
+                 dict_boundaries: Dict[str, str]):
+        """
+        Class used to save the boundary conditions of the simulation.
+
+        :param dict_boundaries: Dictionnary of type of boundaries for each
+            surface.
+        :type dict_boundaries: Dict[str, str]
+        """
+        self.check_conditions(dict_boundaries)
+        self.dict_boundaries = dict_boundaries
+
+    def check_conditions(dict_boundaries: Dict[str, str]):
+        """
+        Check if the boundary conditions respects the naming imposed
+
+        :param dict_boundaries: Dictionnary of type of boundaries for each
+            surface.
+        :type dict_boundaries: Dict[str, str]
+        """
+        if set(dict_boundaries.values) == set(BOUNDARY_CONDITIONS_TYPE):
+            raise InvalidBoundaryConditionError(
+                message=BOUNDARY_CONDITIONS_TYPE - set(dict_boundaries.values))
+
+
+class InvalidBoundaryConditionError(Exception):
+    """
+    Exception raised for invalid boundary conditions in CFD simulations.
+    """
+
+    def __init__(self,
+                 message: str = ''):
+        """
+        :param message: message of the error, defaults to "Invalid boundary
+            condition provided"
+        :type message: str, optional
+        """
+        self.message = "Invalid boundary condition provided" + message
+        super().__init__(self.message)
