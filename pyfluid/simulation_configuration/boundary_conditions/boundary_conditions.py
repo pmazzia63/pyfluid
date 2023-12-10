7b9ff0b1f0840207f9bb827cbfb7b753e378c74f
Paul Mazzia <p.mazzia63@gmail.com>
2023-12-10 17:19:48 +0100

    Implement a first class boundary conditions

--- pyfluid/simulation_configuration/boundary_conditions/boundary_conditions.py
+++ pyfluid/simulation_configuration/boundary_conditions/boundary_conditions.py
@@ -4,32 +4,6 @@ from typing import Dict
 BOUNDARY_CONDITIONS_TYPE = ['pressure', 'speed']
 
 
-class BoundaryConditions:
-    def __init__(self,
-                 dict_boundaries: Dict[str, str]):
-        """
-        Class used to save the boundary conditions of the simulation.
-
-        :param dict_boundaries: Dictionnary of type of boundaries for each
-            surface.
-        :type dict_boundaries: Dict[str, str]
-        """
-        self.check_conditions(dict_boundaries)
-        self.dict_boundaries = dict_boundaries
-
-    def check_conditions(dict_boundaries: Dict[str, str]):
-        """
-        Check if the boundary conditions respects the naming imposed
-
-        :param dict_boundaries: Dictionnary of type of boundaries for each
-            surface.
-        :type dict_boundaries: Dict[str, str]
-        """
-        if set(dict_boundaries.values) == set(BOUNDARY_CONDITIONS_TYPE):
-            raise InvalidBoundaryConditionError(
-                message=BOUNDARY_CONDITIONS_TYPE - set(dict_boundaries.values))
-
-
 class InvalidBoundaryConditionError(Exception):
     """
     Exception raised for invalid boundary conditions in CFD simulations.
@@ -44,3 +18,32 @@ class InvalidBoundaryConditionError(Exception):
         """
         self.message = "Invalid boundary condition provided" + message
         super().__init__(self.message)
+
+
+class BoundaryConditions:
+    def __init__(self,
+                 dict_boundaries: Dict[str, str]):
+        """
+        Class used to save the boundary conditions of the simulation.
+
+        :param dict_boundaries: Dictionnary of type of boundaries for each
+            surface.
+        :type dict_boundaries: Dict[str, str]
+        """
+        self.check_conditions(dict_boundaries)
+        self.dict_boundaries = dict_boundaries
+
+    def check_conditions(self,
+                         dict_boundaries: Dict[str, str]):
+        """
+        Check if the boundary conditions respects the naming imposed
+
+        :param dict_boundaries: Dictionnary of type of boundaries for each
+            surface.
+        :type dict_boundaries: Dict[str, str]
+        """
+        if set(dict_boundaries.values()) != set(BOUNDARY_CONDITIONS_TYPE):
+            raise InvalidBoundaryConditionError(
+                message=str(set(dict_boundaries.values()) -
+                            set(BOUNDARY_CONDITIONS_TYPE))
+            )
